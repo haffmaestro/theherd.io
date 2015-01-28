4bdb59a3843dd37e5569b6c8c88dd149714a0b33
haffmaestro <halfdanhem@gmail.com>
2015-01-28 11:06:19 -0800

    Added Puma as production server, made it so that when a herd is destroyed, everything it owns is also

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,8 @@
 class Section < ActiveRecord::Base
   belongs_to :user_weekly
   belongs_to :focus_area
-  has_many   :weekly_tasks
-  has_many   :comments, -> {order 'created_at ASC'}
+  has_many   :weekly_tasks, dependent: :destroy
+  has_many   :comments, -> {order 'created_at ASC'}, dependent: :destroy
 
   validates :name, presence: true
 end
